87b09db9906c080a3cf112c05559626ee412eda4
Pedro Queiroga <psq@cin.ufpe.br>
2020-04-26 03:30:06 -0300

    Menção de UFRPE e atualização em ajuda

--- src/msgs.py
+++ src/msgs.py
@@ -1,4 +1,4 @@
-ERROR_COULDNT_EXTRACT='Não foi possível extrair as relações de cadeiras deste pdf. Se você realmente enviou um perfil curricular da UFPE, por favor nos informe criando um bug report no github, ou nos contatando.'
+ERROR_COULDNT_EXTRACT='Não foi possível extrair as relações de cadeiras deste pdf. Se você realmente enviou um perfil curricular da UFPE ou UFRPE, por favor nos informe criando um bug report no github, ou nos contatando.'
 
 ERROR_COULDNT_DOWNLOAD='Não foi possível usar a URL fornecida para obter o arquivo pdf. Se o erro persiste e você acredita que enviou uma URL válida, por favor nos informe criando um bug report no github, ou nos contatando.'
 
