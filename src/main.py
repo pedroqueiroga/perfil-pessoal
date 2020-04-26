65ab9b0e3539e227fcd989adf89f2c3d6f878adf
Pedro Queiroga <psq@cin.ufpe.br>
2020-04-26 01:49:26 -0300

    Aceitando pdf de URLs

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import os
 from .perfil2 import do_everything
-
+from . import msgs
+import requests
+import io
+import traceback
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def visualizador():
@@ -10,16 +14,29 @@ def visualizador():
 
 @app.route('/upload', methods = ['POST'])
 def graph():
-    f = request.files['file']
-    if f.content_type != 'application/pdf':
+    if request.form:
+        # devo pegar o arquivo do url
+        url = request.form['url']
+        res = requests.get(url)
+        if res.status_code == requests.codes.ok:
+            content_type = res.headers['Content-Type']
+            f = io.BytesIO(res.content)
+            res.close()
+        else:
+            return render_template('grafo.html',
+                                   error=msgs.ERROR_COULDNT_DOWNLOAD)
+    elif request.files:
+        f = request.files['file']
+        content_type = f.content_type
+    if not 'application/pdf' in content_type:
         request.close()
         return render_template('grafo.html',
-                               error='Não foi possível aceitar que você enviou '
-                               'um pdf.')
+                               error=msgs.ERROR_NOT_PDF)
     try:
         g, curso, universidade, perfil = do_everything(f)
     except Exception as e:
         print('Erro:', e)
+        traceback.print_tb(e.__traceback__)
         g = None
     finally:
         request.close()
@@ -28,7 +45,7 @@ def graph():
         grafo = g.pipe().decode('utf-8')
         return render_template('grafo.html', grafo=grafo, curso=curso, universidade=universidade, perfil=perfil)
     else:
-        return render_template('grafo.html', error='Não foi possível extrair as relações de cadeiras deste pdf. Se você realmente enviou um perfil curricular da UFPE, por favor nos informe criando um bug report no github, ou nos contatando.')
+        return render_template('grafo.html', error=msgs.ERROR_COULDNT_EXTRACT)
 
 @app.route('/contato', methods = ['GET', 'POST'])
 def contato():
@@ -38,7 +55,7 @@ def contato():
         for k in request.form:
             print(k, request.form[k])
         request.close()
-        return render_template('contato.html', obrigado='Obrigado por entrar em contato.')
+        return render_template('contato.html', obrigado=msgs.THANKS_FOR_CONTACT)
     
 
 @app.route('/ajuda', methods = ['GET'])
---
+++ src/msgs.py
@@ -0,0 +1,8 @@
+ERROR_COULDNT_EXTRACT='Não foi possível extrair as relações de cadeiras deste pdf. Se você realmente enviou um perfil curricular da UFPE, por favor nos informe criando um bug report no github, ou nos contatando.'
+
+ERROR_COULDNT_DOWNLOAD='Não foi possível usar a URL fornecida para obter o arquivo pdf. Se o erro persiste e você acredita que enviou uma URL válida, por favor nos informe criando um bug report no github, ou nos contatando.'
+
+ERROR_NOT_PDF='Não foi possível aceitar que você enviou um pdf.'
+
+
+THANKS_FOR_CONTACT='Obrigado por entrar em contato.'
